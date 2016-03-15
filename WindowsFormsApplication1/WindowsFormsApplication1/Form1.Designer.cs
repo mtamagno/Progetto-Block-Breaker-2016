6c78cc6b3ae40afb8e508e47338af40d88c30cf7
MACEL94 <francesco.belacca@hotmail.it>
2016-03-15 18:13:04 +0100

    Finalmente una "pallina"

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,9 +12,60 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+
+        Graphics drawings;
+
         public Form1()
         {
             InitializeComponent();
+            drawings = pictureBox1.CreateGraphics();
         }
+
+        //public class Ball
+        //{
+        //    private void DrawRectangle()
+        //    {
+        //        System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+        //        System.Drawing.Graphics formGraphics;
+        //        formGraphics = Form1.pictureBox1.CreateGraphics();
+        //        formGraphics.DrawRectangle(myPen, new Rectangle(0, 0, 200, 300));
+        //        myPen.Dispose();
+        //        formGraphics.Dispose();
+        //    }
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+            Graphics formGraphics;
+            drawings.FillRectangle(myBrush, new Rectangle(0, 0, 50, 50));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        //private void FillRectangleInt(PaintEventArgs ciao)
+        //{
+
+        //    // Create solid brush.
+        //    SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+        //    // Create location and size of rectangle.
+        //    int x = 0;
+        //    int y = 0;
+        //    int width = 200;
+        //    int height = 200;
+
+        //    // Fill rectangle to screen.
+        //    ciao.Graphics.FillRectangle(blueBrush, x, y, width, height);
+        //}
+        
     }
 }
