80da346525d23bc286823a90caf5bd125ad9ebc3
mtamagno <thegreatherochrono@hotmail.it>
2016-05-02 18:14:00 +0200

    c'e' da usare l evento on resizeend del form2 e da li lanciare l evento on resizeend del form 1 :/

--- WindowsFormsApplication5/WindowsFormsApplication5/Form2.cs
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form2.cs
@@ -17,7 +17,6 @@ namespace WindowsFormsApplication5
         Form3 Start = new Form3();
         Form1 Game = new Form1();
         Panel GamePanels = new Panel();
-        
 
         public Form2()
         {
@@ -50,7 +49,6 @@ namespace WindowsFormsApplication5
             Game.FormBorderStyle = FormBorderStyle.None;
             Game.Anchor = AnchorStyles.Top & AnchorStyles.Bottom & AnchorStyles.Left & AnchorStyles.Right;
             Game.AutoScaleMode = AutoScaleMode.Inherit;
-            Thread prova = new Thread(loadContent);
             //Game.Dock = DockStyle.Fill;
             //Game.Bounds = this.Bounds;
 
@@ -58,24 +56,25 @@ namespace WindowsFormsApplication5
         }
 
 
-        private void loadContent()
-        {
-            while (this.Created)
-            {
-                GamePanels.Height = this.Height;
-                GamePanels.Width = this.Width;
-                Game.Height = this.Height;
-                Game.Width = this.Width;
-
-
-            }
-
-
-        }
-
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
         {
             Game.Close();
+        }
+
+        private void Form2_ResizeEnd(object sender, EventArgs e)
+        {
+            Console.WriteLine(Game.Left);
+            Console.WriteLine(GamePanels.Left);
+            Console.WriteLine(Game.Height);
+            Console.WriteLine(GamePanels.Height);
+            GamePanels.Height = this.Height;
+            GamePanels.Width = this.Width;
+            Game.Height = this.Height;
+            Game.Width = this.Width;
+            GamePanels.Top = 0;
+            GamePanels.Left = 0;           
+            Game.Top = 0;
+            Game.Left = 0;
         }
     }
 }
