17870b4e7d59f2ec1245658b0f51464533140ea3
mtamagno <thegreatherochrono@hotmail.it>
2016-05-19 12:55:50 +0200

    never use a jpg 5000xsomething that is going to be converted in bmp

--- WindowsFormsApplication5/WindowsFormsApplication5/Graphics/Forms/Container.cs
+++ WindowsFormsApplication5/WindowsFormsApplication5/Graphics/Forms/Container.cs
@@ -83,12 +83,12 @@ namespace WindowsFormsApplication5
                     button_start = false;
                     restart_required = true;
                     gameLoop();
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
                     return;
 
                     //termino il thread
                 }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
                 Thread.Sleep(2000);
             }
         }
