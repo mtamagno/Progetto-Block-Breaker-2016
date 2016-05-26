6c30775459ee1386e9d02089596a017ccd0b0d8f
mtamagno <thegreatherochrono@hotmail.it>
2016-05-26 18:16:36 +0200

    Rimesse le funzioni principali in alto senza cancellare le regioni

--- WindowsFormsApplication5/WindowsFormsApplication5/Container2.cs
+++ WindowsFormsApplication5/WindowsFormsApplication5/Container2.cs
@@ -36,34 +36,183 @@ namespace WindowsFormsApplication5
 
         #region Methods
 
-        public void ContinueToMenu(object sender, EventArgs e)
+        private void Container2_Load(object sender, EventArgs e)
         {
-            // Salva prima lo score, poi l'highScore nell'xml
-            this.highScore.Name = gameover.highScore.Name;
-            HighScoreSaver highScoreSaver = new HighScoreSaver();
-            highScoreSaver.ModifyOrCreateXML(highScore);
+            music = new Music();
 
-            // Imposta che il giocatore ha gia finito una partita
-            again = true;
+            // Imposta again a false per dire che il gioco e' stato avviato per la prima volta
+            again = false;
 
-            // Pulisce tutto
-            DisposeAll();
-
-            // Inizializza il gamePanel
+            //inizializzo il pannello che conterra' i form dell'applicazione
             initializeGamePanel();
 
-            // Inizializza il Menu
+            //inizializzo il menu come primo form che apparira' nel gioco
             initializeMenu();
-
-            // Svuota il garbage collector per liberare memoria
-            GC.Collect();
-
-            // Aspetta che il garbage collecor finisca
-            GC.WaitForPendingFinalizers();
-            GC.WaitForFullGCComplete();
         }
 
-        public void DisposeAll()
+        private void initializeGamePanel()
+        {
+            //assegno al pannello un nuovo pannello
+            gamePanels = new Panel();
+
+            // Imposta altezza e lunghezza del pannello
+            gamePanels.Width = this.Width;
+            gamePanels.Height = this.Height;
+
+            // Imposta il metodo Dock per far riempire al pannello tutto lo spazio del form
+            this.Dock = DockStyle.Fill;
+
+            // Imposta la posizione iniziale del pannello
+            gamePanels.Top = 0;
+            gamePanels.Left = 0;
+
+            // Imposta il Dock del pannello per far riempire ai Form figli del pannello tutto lo spazio del pannello
+            gamePanels.Dock = DockStyle.Fill;
+            gamePanels.Anchor = AnchorStyles.Top & AnchorStyles.Bottom & AnchorStyles.Left & AnchorStyles.Right;
+
+            //mostro il pannello
+            gamePanels.Show();
+
+            //aggiungo il pannello al form
+            this.Invoke(new MethodInvoker(delegate
+            {
+                this.Controls.Add(gamePanels);
+            }));
+        }
+
+        private void initializeGame()
+        {   //assegno al gioco un nuovo gioco
+            game = new Game();
+
+            //inizializzo il gioco
+            initializeForm(game);
+
+            //inizializzo il thread per controllare lo stato del gioco
+            initializeThread(gameAlive);
+
+            //faccio partire la musica di gioco
+            music.Game();
+        }
+
+        private void initializeGameOver()
+        {
+            //assegno al gameover un nuovo gameover
+            gameover = new GameOver();
+
+            //inizializzo il gameover
+            initializeForm(gameover);
+
+            //Assegno un testo al pulsante del form gameover
+            gameover.Continue.Text = "Continue";
+
+            //Assegno un evento al pusalnte del gameover
+            gameover.Continue.Click += new EventHandler(this.ContinueToMenu);
+
+            //Faccio partire la musica del gameover
+            music.GameOver();
+        }
+
+        private void initializeMenu()
+        {
+            //assegno al menu un nuovo menu
+            menu = new Start();
+
+            //inizializzo il menu
+            initializeForm(menu);
+
+            //Controllo se e' la prima partita dell utente
+            if (again)
+                menu.start.Text = "Play Again";
+            else
+                menu.start.Text = "Play";
+
+            //Assegno al pulsante del menu un evento
+            menu.start.Click += new EventHandler(this.StartGame);
+
+            //Faccio partire la musica del menu
+            music.Menu();
+        }
+
+        private void initializeForm(Form form)
+        {
+            // Imposta il topLevel del form a false
+            form.TopLevel = false;
+            form.AutoScaleMode = AutoScaleMode.Inherit;
+
+            // Imposta altezza e larghezza del form
+            form.Width = gamePanels.Width;
+            form.Height = gamePanels.Height;
+
+            // Imposta la posizione del form
+            form.Left = 0;
+            form.Top = 0;
+
+            //rimuovo il bordo del form
+            form.FormBorderStyle = FormBorderStyle.None;
+
+            // Imposta il form per occupare tutto lo spazio disponibile
+            form.Dock = DockStyle.Fill;
+            form.Anchor = AnchorStyles.Top & AnchorStyles.Bottom & AnchorStyles.Left & AnchorStyles.Right;
+            form.AutoScaleMode = AutoScaleMode.Inherit;
+
+            //aggiungo e mostro il form a schermo
+            this.Invoke(new MethodInvoker(delegate
+            {
+                gamePanels.Controls.Add(form);
+                form.Show();
+                form.Focus();
+                form.BringToFront();
+            }));
+        }
+
+        private void initializeThread(Thread thread)
+        {   //assegno al thread una funzione
+            gameAlive = new Thread(gameoverCheck);
+
+            //inizializzo il thread
+            gameAlive.Start();
+        }
+
+        //Funzione chiamata quando si sta ridimensionando la finestra
+        private void Container2_ResizeBegin(object sender, EventArgs e)
+        {
+            //Salvo le dimensioni del client prima del ridimensionamento per poter calcolare la proporzione
+            lunghezza_client_iniziale = this.ClientRectangle.Width;
+            altezza_client_iniziale = this.ClientRectangle.Height;
+        }
+
+        // FUnzione chiamata quando viene ridimensionata la finestra
+        private void OnResizeEnd(object sender, EventArgs e)
+        {
+            // Imposta i nuovi valori del gamePanel
+            gamePanels.Height = this.ClientRectangle.Height;
+            gamePanels.Width = this.ClientRectangle.Width;
+            gamePanels.Top = 0;
+            gamePanels.Left = 0;
+
+            //Salvo le dimensioni del client
+            lunghezza_client = this.ClientRectangle.Width;
+            altezza_client = this.ClientRectangle.Height;
+
+            if (game != null)
+            {
+                initializeForm(game);
+                game.on_resize(lunghezza_client_iniziale, altezza_client_iniziale, lunghezza_client, altezza_client);
+                game.ball.totalVelocityReset(lunghezza_client_iniziale, altezza_client_iniziale, lunghezza_client, altezza_client);
+            }
+            if (menu != null)
+            {
+                initializeForm(menu);
+                menu.on_resize(this.Width, this.Height);
+            }
+            if (gameover != null)
+            {
+                initializeForm(gameover);
+                gameover.on_resize(this.Width, this.Height);
+            }
+        }
+
+        private void DisposeAll()
         {
             this.Invoke(new MethodInvoker(delegate
             {
@@ -122,7 +271,35 @@ namespace WindowsFormsApplication5
             }));
         }
 
-        public void gameoverCheck()
+        private void ContinueToMenu(object sender, EventArgs e)
+        {
+            // Salva prima lo score, poi l'highScore nell'xml
+            this.highScore.Name = gameover.highScore.Name;
+            HighScoreSaver highScoreSaver = new HighScoreSaver();
+            highScoreSaver.ModifyOrCreateXML(highScore);
+
+            // Imposta che il giocatore ha gia finito una partita
+            again = true;
+
+            // Pulisce tutto
+            DisposeAll();
+
+            // Inizializza il gamePanel
+            initializeGamePanel();
+
+            // Inizializza il Menu
+            initializeMenu();
+
+            // Svuota il garbage collector per liberare memoria
+            GC.Collect();
+
+            // Aspetta che il garbage collecor finisca
+            GC.WaitForPendingFinalizers();
+            GC.WaitForFullGCComplete();
+        }
+
+
+        private void gameoverCheck()
         {
             //controllo se il game e' ancora attivo
             while (game != null)
@@ -154,7 +331,7 @@ namespace WindowsFormsApplication5
             }
         }
 
-        public void StartGame(object sender, EventArgs e)
+        private void StartGame(object sender, EventArgs e)
         {
             //Pulisco tutto
             DisposeAll();
@@ -183,182 +360,6 @@ namespace WindowsFormsApplication5
             DisposeAll();
             base.OnClosing(e);
             System.Environment.Exit(0);
-        }
-
-        private void Container2_Load(object sender, EventArgs e)
-        {
-            music = new Music();
-
-            // Imposta again a false per dire che il gioco e' stato avviato per la prima volta
-            again = false;
-
-            //inizializzo il pannello che conterra' i form dell'applicazione
-            initializeGamePanel();
-
-            //inizializzo il menu come primo form che apparira' nel gioco
-            initializeMenu();
-        }
-
-        //Funzione chiamata quando si sta ridimensionando la finestra
-        private void Container2_ResizeBegin(object sender, EventArgs e)
-        {
-            //Salvo le dimensioni del client prima del ridimensionamento per poter calcolare la proporzione
-            lunghezza_client_iniziale = this.ClientRectangle.Width;
-            altezza_client_iniziale = this.ClientRectangle.Height;
-        }
-
-        private void initializeForm(Form form)
-        {
-            // Imposta il topLevel del form a false
-            form.TopLevel = false;
-            form.AutoScaleMode = AutoScaleMode.Inherit;
-
-            // Imposta altezza e larghezza del form
-            form.Width = gamePanels.Width;
-            form.Height = gamePanels.Height;
-
-            // Imposta la posizione del form
-            form.Left = 0;
-            form.Top = 0;
-
-            //rimuovo il bordo del form
-            form.FormBorderStyle = FormBorderStyle.None;
-
-            // Imposta il form per occupare tutto lo spazio disponibile
-            form.Dock = DockStyle.Fill;
-            form.Anchor = AnchorStyles.Top & AnchorStyles.Bottom & AnchorStyles.Left & AnchorStyles.Right;
-            form.AutoScaleMode = AutoScaleMode.Inherit;
-
-            //aggiungo e mostro il form a schermo
-            this.Invoke(new MethodInvoker(delegate
-            {
-                gamePanels.Controls.Add(form);
-                form.Show();
-                form.Focus();
-                form.BringToFront();
-            }));
-        }
-
-        private void initializeGame()
-        {   //assegno al gioco un nuovo gioco
-            game = new Game();
-
-            //inizializzo il gioco
-            initializeForm(game);
-
-            //inizializzo il thread per controllare lo stato del gioco
-            initializeThread(gameAlive);
-
-            //faccio partire la musica di gioco
-            music.Game();
-        }
-
-        private void initializeGameOver()
-        {
-            //assegno al gameover un nuovo gameover
-            gameover = new GameOver();
-
-            //inizializzo il gameover
-            initializeForm(gameover);
-
-            //Assegno un testo al pulsante del form gameover
-            gameover.Continue.Text = "Continue";
-
-            //Assegno un evento al pusalnte del gameover
-            gameover.Continue.Click += new EventHandler(this.ContinueToMenu);
-
-            //Faccio partire la musica del gameover
-            music.GameOver();
-        }
-
-        private void initializeGamePanel()
-        {
-            //assegno al pannello un nuovo pannello
-            gamePanels = new Panel();
-
-            // Imposta altezza e lunghezza del pannello
-            gamePanels.Width = this.Width;
-            gamePanels.Height = this.Height;
-
-            // Imposta il metodo Dock per far riempire al pannello tutto lo spazio del form
-            this.Dock = DockStyle.Fill;
-
-            // Imposta la posizione iniziale del pannello
-            gamePanels.Top = 0;
-            gamePanels.Left = 0;
-
-            // Imposta il Dock del pannello per far riempire ai Form figli del pannello tutto lo spazio del pannello
-            gamePanels.Dock = DockStyle.Fill;
-            gamePanels.Anchor = AnchorStyles.Top & AnchorStyles.Bottom & AnchorStyles.Left & AnchorStyles.Right;
-
-            //mostro il pannello
-            gamePanels.Show();
-
-            //aggiungo il pannello al form
-            this.Invoke(new MethodInvoker(delegate
-            {
-                this.Controls.Add(gamePanels);
-            }));
-        }
-
-        private void initializeMenu()
-        {
-            //assegno al menu un nuovo menu
-            menu = new Start();
-
-            //inizializzo il menu
-            initializeForm(menu);
-
-            //Controllo se e' la prima partita dell utente
-            if (again)
-                menu.start.Text = "Play Again";
-            else
-                menu.start.Text = "Play";
-
-            //Assegno al pulsante del menu un evento
-            menu.start.Click += new EventHandler(this.StartGame);
-
-            //Faccio partire la musica del menu
-            music.Menu();
-        }
-
-        private void initializeThread(Thread thread)
-        {   //assegno al thread una funzione
-            gameAlive = new Thread(gameoverCheck);
-
-            //inizializzo il thread
-            gameAlive.Start();
-        }
-
-        // FUnzione chiamata quando viene ridimensionata la finestra
-        private void OnResizeEnd(object sender, EventArgs e)
-        {
-            // Imposta i nuovi valori del gamePanel
-            gamePanels.Height = this.ClientRectangle.Height;
-            gamePanels.Width = this.ClientRectangle.Width;
-            gamePanels.Top = 0;
-            gamePanels.Left = 0;
-
-            //Salvo le dimensioni del client
-            lunghezza_client = this.ClientRectangle.Width;
-            altezza_client = this.ClientRectangle.Height;
-
-            if (game != null)
-            {
-                initializeForm(game);
-                game.on_resize(lunghezza_client_iniziale, altezza_client_iniziale, lunghezza_client, altezza_client);
-                game.ball.totalVelocityReset(lunghezza_client_iniziale, altezza_client_iniziale, lunghezza_client, altezza_client);
-            }
-            if (menu != null)
-            {
-                initializeForm(menu);
-                menu.on_resize(this.Width, this.Height);
-            }
-            if (gameover != null)
-            {
-                initializeForm(gameover);
-                gameover.on_resize(this.Width, this.Height);
-            }
         }
 
         #endregion Methods
