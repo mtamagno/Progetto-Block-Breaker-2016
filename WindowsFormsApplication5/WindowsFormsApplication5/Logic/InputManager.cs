87eefd3e2069636191be81edf298d97518f0d962
mtamagno <thegreatherochrono@hotmail.it>
2016-06-23 15:16:13 +0200

    gah

--- WindowsFormsApplication5/WindowsFormsApplication5/Logic/InputManager.cs
+++ WindowsFormsApplication5/WindowsFormsApplication5/Logic/InputManager.cs
@@ -9,11 +9,25 @@ namespace BlockBreaker
     {
         #region Public Fields
 
+        public float DeltaTime;
+        public Stopwatch GameTime;
         public List<Sprite> InGameSprites = new List<Sprite>();
+        public Keys[] KeyHeld;
+        public Keys[] KeyPressed;
+        public Point MousePoint;
 
         #endregion Public Fields
 
         #region Public Methods
+
+        public void update(Point mp, Keys[] kp, Keys[] kh, Stopwatch gt, float dt)
+        {
+            MousePoint = mp;
+            KeyPressed = kp;
+            KeyHeld = kh;
+            GameTime = gt;
+            DeltaTime = dt / 1000f;
+        }
 
         #endregion Public Methods
     }
